db290b21141fe5412ce6554b70605f02960f585a
Brit Wallace <britwallace888@gmail.com>
2022-01-16 17:03:10 -0800

    Add species and info about pokemon
    Co-authored-by: Nathan Conn <nconn34@gmail.com>
    Co-authored-by: Ryan Rendon<Rendon.S.Ryan@gmail.com>
    Co-authored-by: Karl Starkweather <stark13@usa.net>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
-import PokemonName from './pokemon-service.js'
+import { PokemonName, PokemonSpecies } from './pokemon-service.js'
 
 
 function clearFields() {
@@ -13,23 +13,56 @@ function clearFields() {
     $('.showImg').text("");
   }
   
-  function getInfo(response) {
+  function getInfo(response, species) {
     if (response.forms) {
       $('.showName').text(`${response.forms[0].name}`);
       $('.showImg').html(`<img src=${response.sprites.other.dream_world.front_default}>`);
-      // $('.showHabitat').text(`${response.}`)
+      $('.showAbilities').text(`Abilities: ${getAbilities(response.abilities)}`);
+      $('.showMoves').text(`Moves: ${getMoves(response.moves)}`);
+      $('.showTypes').text(`Types: ${getTypes(response.types)}`);
+      $('.showEggs').text(`Egg Group: ${species.egg_groups[0].name}`);
       } else {
       $('.showErrors').text(`There was an error: ${response}`);
     }
   }
+
+  function getMoves(movesArray){
+    let moves = "";
+    for (let i = 0; i <movesArray.length; i++){
+      moves += movesArray[i].move.name + ", ";
+    }
+    return moves
+}
+
+  function getTypes(typesArray) {
+    let types = "";
+    for (let i = 0; i <typesArray.length; i++){
+      types += typesArray[i].type.name + ", ";
+    }
+    return types
+  }
+
+  function getAbilities(abilitiesArray) {
+    let ability = "";
+    for (let i = 0; i <abilitiesArray.length; i++){
+      ability += abilitiesArray[i].ability.name + ", ";
+    }
+    return ability
+  }
   
   async function makeApiCall(number) {
     const response = await PokemonName.getPokemon(number);
-    getInfo(response);
+    const species = await PokemonSpecies.getSpecies(name);
+    getInfo(response, species);
   }
   
   $(document).ready(function() {
     $('#pokemonID').click(function() {
+      let pokemon = $('#pokemonNum').val();
+      clearFields();
+      makeApiCall(pokemon);
+    });
+  });
       // let all = [];
       // for(i=1;i<897;i++) {
       //   all.push(i);
@@ -46,6 +79,6 @@ function clearFields() {
           
       //   }
       // }
-    });
-  });
+  //   });
+  // });
 
--- src/pokemon-service.js
+++ src/pokemon-service.js
@@ -1,4 +1,4 @@
-export default class PokemonName{
+export class PokemonName{
   static async getPokemon(name){
       return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
       .then(function(response) {
@@ -25,6 +25,21 @@ export default class PokemonName{
     })
 }
 
+}
+
+export class PokemonSpecies{
+  static async getSpecies(name){
+      return fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
+  }
 }
 
 // export class PokemonHabitat{
