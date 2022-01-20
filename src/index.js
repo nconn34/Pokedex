791d933e1bcc463ebf77c9a68fd66e1b6717a60e
Nathan Conn <nconn34@gmail.com>
2022-01-19 20:41:13 -0800

    Refactored time travel

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
-import { PokemonName, PokemonSpecies } from './pokemon-service.js'
+import { Pokemon } from './pokemon-service.js'
 import 'animate.css';
 
 
@@ -19,18 +19,18 @@ function clearFields() {
    $('.showFlavorText').text("");
   }
   
-  function getInfo(response, species) {
+  function loadInfo(response, species) {
     if (response.forms) {
       $('.name').text(`${response.forms[0].name}`);
       $('.card-title').html(`<img src=${response.sprites.other.dream_world.front_default}>`);
       $('.type').text(`${response.types[0].type.name}`);
       $('.height').text(`${response.height}`);
       $('.weight').text(`${response.weight}`);
-      $('.showAbilities').text(`${getAbilities(response.abilities)}`);
-      $('.showMoves').text(`${getMoves(response.moves)}`);
-      $('.showTypes').text(`Types: ${getTypes(response.types)}`);
+      // $('.showAbilities').text(`${getAbilities(response.abilities)}`);
+      // $('.showMoves').text(`${getMoves(response.moves)}`);
+      // $('.showTypes').text(`Types: ${getPokeTypes(response.types)}`);
       $('.showEggs').text(`Egg Group: ${getEggs(species.egg_groups)}`);
-     $('.showHabitat').text(`Habitat: ${species.habitat.name}`);
+     $('.showPokeHabitat').text(`Habitat: ${species.habitat.name}`);
      $('.showFlavorText').text(`Prof. Oak Says: ${species.flavor_text_entries[1].flavor_text}`);
       } else {
       $('.showErrors').text(`There was an error: ${response}`);
@@ -42,29 +42,29 @@ function clearFields() {
     $('.back-of-card').toggle();
   }
 
-  function getMoves(movesArray){
-    let moves = "";
-    for (let i = 0; i <movesArray.length; i++){
-      moves += movesArray[i].move.name + ", ";
-    }
-    return moves
-}
+//   function getMoves(movesArray){
+//     let moves = "";
+//     for (let i = 0; i <movesArray.length; i++){
+//       moves += movesArray[i].move.name + ", ";
+//     }
+//     return moves
+// }
 
-  function getTypes(typesArray) {
-    let types = "";
-    for (let i = 0; i <typesArray.length; i++){
-      types += typesArray[i].type.name + ", ";
-    }
-    return types
-  }
+//   function getPokeTypes(typesArray) {
+//     let types = "";
+//     for (let i = 0; i <typesArray.length; i++){
+//       types += typesArray[i].type.name + ", ";
+//     }
+//     return types
+//   }
 
-  function getAbilities(abilitiesArray) {
-    let ability = "";
-    for (let i = 0; i <abilitiesArray.length; i++){
-      ability += abilitiesArray[i].ability.name + ", ";
-    }
-    return ability
-  }
+//   function getAbilities(abilitiesArray) {
+//     let ability = "";
+//     for (let i = 0; i <abilitiesArray.length; i++){
+//       ability += abilitiesArray[i].ability.name + ", ";
+//     }
+//     return ability
+//   }
 
   function getEggs(eggsArray) {
     let eggs = "";
@@ -75,14 +75,35 @@ function clearFields() {
     return eggs
   }
   
-  async function makeApiCall(name) {
-    const response = await PokemonName.getPokemon(name);
-    const species = await PokemonSpecies.getSpecies(name);
-    getInfo(response, species);
+  function filterHabitat(habitat){
+    return fetch(`https://pokeapi.co/api/v2/pokemon-habitat/${habitat}`)
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch(function(error) {
+      return error;
+    })
   }
   
-  async function getHabitat(habitat) {
-      const response = await PokemonName.filterHabitat(habitat);
+  function filterType(type){
+    return fetch(`https://pokeapi.co/api/v2/type/${type}`)
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch(function(error) {
+      return error;
+    })
+  }
+
+
+  async function getPokeWithHabitat(habitat) {
+      const response = await filterHabitat(habitat);
       let habitatList = [];
       for(let i=0;i<response.pokemon_species.length;i++) {
         habitatList.push(response.pokemon_species[i].name);
@@ -91,8 +112,8 @@ function clearFields() {
       return habitatList;
   }
 
-  async function getType(type) {
-    const response = await PokemonName.filterType(type)
+  async function getPokeWithType(type) {
+    const response = await filterType(type)
     let typeList = [];
     for(let i=0;i<response.pokemon.length;i++) {
       typeList.push(response.pokemon[i].name);
@@ -103,25 +124,35 @@ function clearFields() {
 
   $(document).ready(function() {
     $('#pokemonID').click(function() {
-      let pokemon = $('#pokemonNum').val();
+      let pName = $('#pokemonName').val();
       clearFields();
-      makeApiCall(pokemon);
+      let poke = new Pokemon(pName);
+      console.log(poke);
+      console.log(poke);
+      poke.loadInfo();
+      console.log(poke.info.forms[0].name);
+      console.log(poke.speciesInfo);
     });
-    $('#moreInfo').click(function() {
-      flipCard();
-    });
+    // $('#moreInfo').click(function() {
+    //   flipCard();
+    // });
   
-    $('#selectHabitat').change(function() {
-      let selectedHabitat = $('#selectHabitat').val();
-      const habitatList = getHabitat(selectedHabitat);
-      console.log(habitatList)
-    })
-    $('#selectType').change(function() {
-      let selectedType = $('#selectType').val();
-      const typeList = getType(selectedType);
-      console.log(typeList)
-    })
+    // $('#selectHabitat').change(function() {
+    //   let selectedHabitat = $('#selectHabitat').val();
+    //   const habitatList = getPokeWithHabitat(selectedHabitat);
+    //   console.log(habitatList)
+    // })
+    // $('#selectType').change(function() {
+    //   let selectedType = $('#selectType').val();
+    //   const typeList = getPokeWithType(selectedType);
+    //   console.log(typeList)
+    // })
   });
+
+
+
+
+
       // let all = [];
       // for(i=1;i<897;i++) {
       //   all.push(i);
--- src/pokemon-service.js
+++ src/pokemon-service.js
@@ -1,58 +1,67 @@
-export class PokemonName{
-  static async getPokemon(name){
-      return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      })
+export class Pokemon{
+  constructor(name) {
+    this.name = name;
+    this.info = {};
+    this.speciesInfo = {};
   }
 
-  static async filterHabitat(habitat){
-    return fetch(`https://pokeapi.co/api/v2/pokemon-habitat/${habitat}`)
+  async getPokemonInfo(){
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${this.name}`)
     .then(function(response) {
       if (!response.ok) {
         throw Error(response.statusText);
       }
       return response.json();
     })
-    .catch(function(error) {
-      return error;
-    })
+    .catch(function(error) {return error;})
   }
-  static async filterType(type){
-    return fetch(`https://pokeapi.co/api/v2/type/${type}`)
+
+  async getSpeciesInfo(){
+    return fetch(`https://pokeapi.co/api/v2/pokemon-species/${this.name}`)
     .then(function(response) {
       if (!response.ok) {
         throw Error(response.statusText);
       }
       return response.json();
     })
-    .catch(function(error) {
-      return error;
-    })
+    .catch(function(error) {return error;})
+  }
+
+  async loadInfo() {
+    this.info = await this.getPokemonInfo();
+    this.speciesInfo = await this.getSpeciesInfo();
+  }
+
+  getMoves(movesArray){
+    let moves = "";
+    for (let i = 0; i <movesArray.length; i++){
+      moves += movesArray[i].move.name + ", ";
+    }
+    return moves
+  }
+
+  getPokeTypes(typesArray) {
+    let types = "";
+    for (let i = 0; i <typesArray.length; i++){
+      types += typesArray[i].type.name + ", ";
+    }
+    return types
+  }
+
+  getAbilities(abilitiesArray) {
+    let ability = "";
+    for (let i = 0; i <abilitiesArray.length; i++){
+      ability += abilitiesArray[i].ability.name + ", ";
+    }
+    return ability
   }
 
 }
 
-export class PokemonSpecies{
-  static async getSpecies(name){
-      return fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      })
-  }
-}
+
+
+
+
 
 // export class PokemonHabitat{
 //   static async getHabitat(name){
